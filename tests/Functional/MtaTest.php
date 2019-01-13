5de962c4962c58aab07204399630e3b755be4e06
Emiliano Castro <emilianocast98@gmail.com>
2019-01-13 20:37:06 -0300

    Add Travis-CI

---
+++ tests/Functional/MtaTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class MtaTest extends TestCase
+{
+    /**
+     * @todo TestCase
+     */
+    public function testToDoTest()
+    {
+        $this->assertTrue(true);
+    }
+}
